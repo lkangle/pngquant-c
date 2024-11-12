a79f30a6a036b8d224363c661b48e80ef5610be9
likangle <likangle@corp.netease.com>
2024-11-12 15:04:30 +0800

    feat: m1支持openmp

--- lib/viter.c
+++ lib/viter.c
@@ -72,7 +72,9 @@ LIQ_PRIVATE double viter_do_iteration(histogram *hist, colormap *const map, cons
 
     double total_diff=0;
     #pragma omp parallel for if (hist_size > 3000) \
-        schedule(static) default(none) shared(average_color,callback) reduction(+:total_diff)
+        schedule(static) default(none) \
+        shared(average_color, callback, hist_size, n, achv, min_opaque_val, map) \
+        reduction(+:total_diff)
     for(int j=0; j < hist_size; j++) {
         float diff;
         unsigned int match = nearest_search(n, achv[j].acolor, achv[j].tmp.likely_colormap_index, min_opaque_val, &diff);
