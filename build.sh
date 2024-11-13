ab4f6e180d13786ef9690f19169285791ee6427e
likangle <likangle@corp.netease.com>
2024-11-13 18:38:36 +0800

    fix: macos

--- build.sh
+++ build.sh
@@ -5,14 +5,23 @@ ROOT=$(pwd)
 MAKE=make
 CMAKE_G="Unix Makefiles"
 ZLIB="libz.so"
+CC=gcc
+SIMD_OPTION="--enable-sse"
 
-if [[ "$(uname -s)" == "MINGW"* ]]; then
+OS=$(uname -s)
+
+if [[ "$OS" == "MINGW"* ]]; then
     MAKE="mingw32-make"
     CMAKE_G="MinGW Makefiles"
     # ZLIB="libz.a"
     echo "This is a Windows environment ${MAKE} ${CMAKE_G}"
+elif [[ "$OS" == "Darwin" ]]; then
+    CC=gcc-14
+    SIMD_OPTION="--disable-sse --enable-neon"
+    ZLIB="libz.a"
+    echo "This is a Macos environment"
 else
-    echo "This is not a Windows environment"
+    echo "This is a Linux environment"
 fi
 
 # build zlib 1.3.1
@@ -25,18 +34,23 @@ else
     $MAKE
 fi
 
-# build libpng 1.6.17
-if [ -f "$ROOT/libpng-1.6.17/build/libpng.a" ]; then
-    echo "libpng.a already exists, skipping libpng build."
-else
-    cd $ROOT/libpng-1.6.17
-    mkdir build
-    cd build
-    cmake -G "${CMAKE_G}" -DPNG_STATIC=ON -DZLIB_LIBRARY=$ROOT/zlib-1.3.1/$ZLIB -DZLIB_INCLUDE_DIR=$ROOT/zlib-1.3.1 ..
-    $MAKE
+if [[ "$OS" != "Darwin" ]]; then
+    # build libpng 1.6.17
+    if [ -f "$ROOT/libpng-1.6.17/build/libpng.a" ]; then
+        echo "libpng.a already exists, skipping libpng build."
+    else
+        cd $ROOT/libpng-1.6.17
+        rm -fr build
+        mkdir build
+        cd build
+        cmake -G "${CMAKE_G}" -DPNG_STATIC=ON -DZLIB_LIBRARY=$ROOT/zlib-1.3.1/$ZLIB -DZLIB_INCLUDE_DIR=$ROOT/zlib-1.3.1 ..
+        $MAKE
+    fi
+else 
+    echo "macos, skipping libpng build."
 fi
 
 cd $ROOT
 chmod +x $ROOT/configure
-./configure --with-openmp --enable-sse --without-cocoa --without-lcms2
+./configure --with-openmp ${SIMD_OPTION} --without-cocoa --without-lcms2 CC=${CC}
 $MAKE
